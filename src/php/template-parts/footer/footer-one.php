0f88abf85a85b461bb47b7fc9b2843b4d428e7a1
Ilya <36233549+cyberpunk3351@users.noreply.github.com>
2020-02-07 21:58:38 +0700

    footerr block

--- src/php/template-parts/footer/footer-one.php
+++ src/php/template-parts/footer/footer-one.php
@@ -1,5 +1,21 @@
 <div class="wrapper">
-    <div class="footer-main"></div>
+    <div class="footer-main">
+        <div class="containers">
+            <div class="footer-main-one">
+                <p>Menu</p>
+                    <ul>
+                        <li>Main</li>
+                        <li>Articles</li>
+                    </ul>
+            </div>
+            <div class="footer-main-two">
+                <p>Popular Post</p>
+            </div>
+            <div class="footer-main-three">
+                <p>Social</p>
+            </div>
+        </div>
+    </div>
     <div class="footer-bottom">
         <div class="containers">
             <p>Plane of the Breaks</p>
