8460e751bfd25601cf0abd4d0d32db067668276f
Ilya <36233549+cyberpunk3351@users.noreply.github.com>
2020-02-09 20:58:30 +0700

    edit conent block

--- src/php/template-parts/content/content.php
+++ src/php/template-parts/content/content.php
@@ -1,18 +1,23 @@
 <div class="contentAll">
-    <?php if (have_posts()) :
-    while (have_posts()) : the_post(); ?>            
-        <div class="info__left_shortNews">
-            <?php the_post_thumbnail('thumbnail'); ?>
-            <div class="info__left_shortNews_author">
-                <h1><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-                <p><?php the_time("d M Y"); ?></p>
-                <!-- <p><?php the_author() ?></p> -->
-                <p><?php the_excerpt() ?></p>
-                <p><a href="<?php the_permalink() ?>">Читать</a></p>
+
+    <div class="contentAll__left">
+        <?php if (have_posts()) :
+        while (have_posts()) : the_post(); ?>            
+            <div class="info__left_shortNews">
+                <?php the_post_thumbnail('thumbnail'); ?>
+                <div class="info__left_shortNews_author">
+                    <h1><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+                    <p><?php the_time("d M Y"); ?></p>
+                    <!-- <p><?php the_author() ?></p> -->
+                    <p><?php the_excerpt() ?></p>
+                    <p><a href="<?php the_permalink() ?>">Читать</a></p>
+                </div>
             </div>
-        </div>
-        <?php endwhile; ?>
-    <?php else : ?>
-        <p>Non d sdf sde</p>
-    <?php endif; ?>
+            <?php endwhile; ?>
+        <?php else : ?>
+            <p>Non d sdf sde</p>
+        <?php endif; ?>
+    </div>
+    <div class="contentAll__right"></div>
+
 </div>
