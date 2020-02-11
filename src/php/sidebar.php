ad2f6dccfa6a01ac7c300ef470b6ff246d2a4b76
Ilya <36233549+cyberpunk3351@users.noreply.github.com>
2020-02-11 23:19:43 +0700

    edit side bar

--- src/php/sidebar.php
+++ src/php/sidebar.php
@@ -1,3 +1,13 @@
 <div class="sidebar">
-    <?php dynamic_sidebar('sidebar-1'); ?>
+    <div class="sidebar__recent">
+        <div class="sidebar__recent--title">
+            <p>RECENT POST</p>
+        </div>
+        <div class="sidebar__recent__trimonth">
+            <div class="sidebar__recent__trimonth--one"></div>
+            <div class="sidebar__recent__trimonth--two"></div>
+            <div class="sidebar__recent__trimonth--three"></div>
+        </div>
+    </div>
+    <!-- <?php dynamic_sidebar('sidebar-1'); ?> -->
 </div>
