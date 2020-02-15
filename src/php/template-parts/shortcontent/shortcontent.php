e051a276a72ddd7b450f84827c5f0c2a16287de2
Ilya <36233549+cyberpunk3351@users.noreply.github.com>
2020-02-15 22:58:27 +0700

    edit  short news

--- src/php/template-parts/shortcontent/shortcontent.php
+++ src/php/template-parts/shortcontent/shortcontent.php
@@ -1,4 +1,3 @@
-
 <div class="contentAll__left--top"></div>
 <?php if (have_posts()) :
 while (have_posts()) : the_post(); ?>            
@@ -6,10 +5,10 @@ while (have_posts()) : the_post(); ?>
         <?php the_post_thumbnail('thumbnail'); ?>
         <div class="info__left_shortNews_author">
             <h1><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-            <p><?php the_time("d M Y"); ?></p>
-            <!-- <p><?php the_author() ?></p> -->
+            <span><?php the_time("d M Y"); ?>,</span>
+            <span><?php the_author() ?></span>
             <p><?php the_excerpt() ?></p>
-            <p><a href="<?php the_permalink() ?>">Читать</a></p>
+            <span class="linkMore"><a href="<?php the_permalink() ?>">Читать</a></span>
         </div>
     </div>
     <?php endwhile; ?>
